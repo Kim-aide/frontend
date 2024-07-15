e87bc86042a0f310b115aff8bdaf0e28f676bca7
WizardRabbit <curious.wzrabbit@gmail.com>
2024-07-16 00:00:27 +0900

    chore: 프로젝트 전체에 적용시킬 글로벌 스타일 파일 생성
    Co-authored-by: baesee0806 <baesee@kakao.com>

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Global, css } from '@emotion/react';
+
+const baseStyle = css`
+	* {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		font-family: Pretendard;
+		box-sizing: border-box;
+	}
+
+	button {
+		border: 0;
+
+		cursor: pointer;
+	}
+
+	button:disabled {
+		cursor: not-allowed;
+	}
+
+	input::-webkit-outer-spin-button,
+	input::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+
+	input[type='number'] {
+		-moz-appearance: textfield;
+		appearance: textfield;
+	}
+
+	body:has([role='dialog']) {
+		overflow: hidden;
+	}
+`;
+
+const GlobalStyle = () => <Global styles={baseStyle} />;
+
+export default GlobalStyle;
