1b172aa501ccde4fd01df7e48769e5473f7b18af
FE_Wani <62400479+baesee0806@users.noreply.github.com>
2024-07-19 20:35:30 +0900

    feat: 공용 로그인 버튼 구현

---
+++ src/components/loginBtns/LoginBtn.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import LoginBtn from './LoginBtn';
+
+/**
+ * 소셜 로그인 공용 버튼 스토리입니다.
+ *
+ * - `type`: 소셜 로그인 버튼 타입을 설정합니다.(ex: google, naver)
+ */
+
+const meta: Meta<typeof LoginBtn> = {
+	title: 'Components/LoginBtn',
+	component: LoginBtn,
+	tags: ['autodocs'],
+};
+export default meta;
+
+type Story = StoryObj<typeof LoginBtn>;
+
+export const Google: Story = {
+	args: {
+		type: 'google',
+	},
+};
+
+export const Naver: Story = {
+	args: {
+		type: 'naver',
+	},
+};
