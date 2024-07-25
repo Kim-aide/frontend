f6b3e023a61148ab0ede46e896056e9fc8549666
WizardRabbit <curious.wzrabbit@gmail.com>
2024-07-25 21:42:56 +0900

    refactor: 사람 svg 아이콘을 사용하도록 변경하고, 기존 svg 컴포넌트를 제거

--- src/components/common/IconBtn/IconBtn.stories.tsx
+++ src/components/common/IconBtn/IconBtn.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import IconBtn from './IconBtn';
+import { PeopleIcon } from '../../../assets/svg';
 
 /**
  * `IconBtn`은 아이콘 형태의 공용 버튼 컴포넌트입니다.
@@ -33,36 +34,6 @@ const meta = {
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-
-const PeopleIcon = (props: SVGProps<SVGSVGElement>) => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="1em"
-			height="1em"
-			viewBox="0 0 24 24"
-			{...props}
-		>
-			<path
-				fill="currentColor"
-				fillRule="evenodd"
-				d="M16.67 13.13C18.04 14.06 19 15.32 19 17v3h4v-3c0-2.18-3.57-3.47-6.33-3.87"
-			></path>
-			<circle
-				cx="9"
-				cy="8"
-				r="4"
-				fill="currentColor"
-				fillRule="evenodd"
-			></circle>
-			<path
-				fill="currentColor"
-				fillRule="evenodd"
-				d="M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4c-.47 0-.91.1-1.33.24a5.98 5.98 0 0 1 0 7.52c.42.14.86.24 1.33.24m-6 1c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4"
-			></path>
-		</svg>
-	);
-};
 
 export const Medium: Story = {
 	args: {
