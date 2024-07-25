928075d6d3a3236cf149dbc4e904ffdc252481ba
WizardRabbit <curious.wzrabbit@gmail.com>
2024-07-25 22:03:21 +0900

    refactor: 마이크 svg 아이콘을 import하여 사용하도록 변경하고, 기존에 직접 선언하여 사용하던 마이크 svg 컴포넌트를 제거

--- src/components/common/IconToggleBtn/IconToggleBtn.stories.tsx
+++ src/components/common/IconToggleBtn/IconToggleBtn.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import IconToggleBtn from './IconToggleBtn';
+import { MicrophoneIcon, MicrophoneOffIcon } from '../../../assets/svg';
 
 /**
  * `IconToggleBtn`은 토글링이 가능한, 특정 상태가 활성화/비활성화 되어 있는지를 시각적으로 보여주는 공용 버튼 컴포넌트입니다.
@@ -39,44 +40,10 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const MicrophoneIcon = (props: SVGProps<SVGSVGElement>) => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="1em"
-			height="1em"
-			viewBox="0 0 24 24"
-			{...props}
-		>
-			<path
-				fill="currentColor"
-				d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3m5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72z"
-			></path>
-		</svg>
-	);
-};
-
-const MicrophoneMutedIcon = (props: SVGProps<SVGSVGElement>) => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="1em"
-			height="1em"
-			viewBox="0 0 24 24"
-			{...props}
-		>
-			<path
-				fill="currentColor"
-				d="M19 11h-1.7c0 .74-.16 1.43-.43 2.05l1.23 1.23c.56-.98.9-2.09.9-3.28m-4.02.17c0-.06.02-.11.02-.17V5c0-1.66-1.34-3-3-3S9 3.34 9 5v.18zM4.27 3L3 4.27l6.01 6.01V11c0 1.66 1.33 3 2.99 3c.22 0 .44-.03.65-.08l1.66 1.66c-.71.33-1.5.52-2.31.52c-2.76 0-5.3-2.1-5.3-5.1H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c.91-.13 1.77-.45 2.54-.9L19.73 21L21 19.73z"
-			></path>
-		</svg>
-	);
-};
-
 export const Active: Story = {
 	args: {
 		activeIcon: <MicrophoneIcon />,
-		inactiveIcon: <MicrophoneMutedIcon />,
+		inactiveIcon: <MicrophoneOffIcon />,
 		isActive: true,
 		ariaLabel: '마이크 켜기',
 	},
@@ -85,7 +52,7 @@ export const Active: Story = {
 export const Inactive: Story = {
 	args: {
 		activeIcon: <MicrophoneIcon />,
-		inactiveIcon: <MicrophoneMutedIcon />,
+		inactiveIcon: <MicrophoneOffIcon />,
 		isActive: false,
 		ariaLabel: '마이크 켜기',
 	},
@@ -94,7 +61,7 @@ export const Inactive: Story = {
 export const ActiveDisabled: Story = {
 	args: {
 		activeIcon: <MicrophoneIcon />,
-		inactiveIcon: <MicrophoneMutedIcon />,
+		inactiveIcon: <MicrophoneOffIcon />,
 		isActive: true,
 		disabled: true,
 		ariaLabel: '마이크 켜기',
@@ -104,7 +71,7 @@ export const ActiveDisabled: Story = {
 export const InactiveDisabled: Story = {
 	args: {
 		activeIcon: <MicrophoneIcon />,
-		inactiveIcon: <MicrophoneMutedIcon />,
+		inactiveIcon: <MicrophoneOffIcon />,
 		isActive: false,
 		disabled: true,
 		ariaLabel: '마이크 켜기',
