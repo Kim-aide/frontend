ebc049e0cdee8e8b230300b41200de64c2bd73b4
WizardRabbit <curious.wzrabbit@gmail.com>
2024-07-27 01:08:26 +0900

    design: 말풍선, 이모지, 종이 비행기 svg 아이콘 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,1 +1,4 @@
 export { ReactComponent as PeopleIcon } from './people-icon.svg';
+export { ReactComponent as ChatBubbleIcon } from './chat-bubble-icon.svg';
+export { ReactComponent as EmojiIcon } from './emoji-icon.svg';
+export { ReactComponent as PaperPlaneIcon } from './paper-plane-icon.svg';
