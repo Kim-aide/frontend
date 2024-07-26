e2ea3af8b94ac1466cc83fb6b4fcd20a484905ef
WizardRabbit <curious.wzrabbit@gmail.com>
2024-07-27 01:07:02 +0900

    design: 반투명한 백색, 어두운 회색, 토마토 색을 theme에 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,6 +4,9 @@ export const theme = {
 		BLACK: '#000000',
 		NAVER_BACKGROUND: '#02C75A',
 		WHITE: '#FFFFFF',
+		WHITE_TRANSPARENT: '#ffffff90',
 		LIGHT_GRAY: '#BBBBBB',
+		DARK_GRAY: '#212121',
+		TOMATO: '#ff4040',
 	},
 };
