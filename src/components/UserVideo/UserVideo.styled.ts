e7b4afee130f709b6e40eb5affb635bc379825ce
요술토끼 <curious.wzrabbit@gmail.com>
2024-08-05 16:40:52 +0900

    feat: UserVideo 컴포넌트 생성, 캠 켜진 화면/꺼진 화면, 그리고 녹색으로 하이라이팅되는 UI를 구현
    - 실제 streamManager가 주어짐을 생각해 두고 구현

---
+++ src/components/UserVideo/UserVideo.styled.ts
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+const Container = styled.div<{
+	$width: string;
+	$height: string;
+	$videoActive: boolean;
+	$isUserSpeaking: boolean;
+}>`
+	overflow: hidden;
+	position: relative;
+	width: ${({ $width }) => $width};
+	height: ${({ $height }) => $height};
+	border-radius: 8px;
+	background-color: ${({ theme }) => theme.colors.BLACK};
+	${({ theme, $isUserSpeaking }) =>
+		$isUserSpeaking
+			? css`
+					outline: 4px solid ${theme.colors.GREEN};
+					box-shadow: 0 0 20px ${theme.colors.GREEN};
+				`
+			: css`
+					outline: 0 solid ${theme.colors.BLACK};
+				`};
+`;
+
+const NoVideoScreen = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+	background-color: ${({ theme }) => theme.colors.BLACK};
+
+	& > img {
+		max-width: 30%;
+		max-height: 30%;
+	}
+`;
+
+const Video = styled.video<{ $videoActive: boolean }>`
+	display: ${({ $videoActive }) => ($videoActive ? 'block' : 'none')};
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+`;
+
+export { Container, NoVideoScreen, Video };
---
+++ src/components/UserVideo/UserVideo.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import * as S from './UserVideo.styled';
+import type { StreamManager } from 'openvidu-browser';
+import useStreamManagerProperties from '../../hooks/useStreamManagerProperties';
+import { kimAideLogoLarge } from '../../assets/png';
+
+interface UserVideoProps {
+	streamManager: StreamManager;
+	width: string;
+	height: string;
+}
+
+const UserVideo = ({ streamManager, width, height }: UserVideoProps) => {
+	const { videoRef, videoActive, isUserSpeaking } = useStreamManagerProperties({
+		streamManager,
+	});
+
+	return (
+		<S.Container
+			$width={width}
+			$height={height}
+			$videoActive={videoActive}
+			$isUserSpeaking={isUserSpeaking}
+		>
+			<S.NoVideoScreen>
+				<img src={kimAideLogoLarge} alt="" />
+			</S.NoVideoScreen>
+			<S.Video
+				autoPlay={true}
+				ref={videoRef}
+				$videoActive={videoActive}
+			></S.Video>
+		</S.Container>
+	);
+};
+
+export default UserVideo;
