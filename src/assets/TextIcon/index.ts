2c1cf067b6a90f532b2407ec86e82b40d4f920a1
FE_Wani <baesee@kakao.com>
2024-08-13 16:50:36 +0900

    feat: IconTextBtn에 들어가는 svg 파일 추가
    - 4개의 이미지 파일 추가 아직 정해진 틀이 없어서 추후 변경 할려고 합니다.
    
    Relate to: #2

---
+++ src/assets/TextIcon/index.ts
@@ -0,0 +1,7 @@
+export { ReactComponent as AddMeetingIcon } from './add-call.svg';
+
+export { ReactComponent as SearchIcon } from './search.svg';
+
+export { ReactComponent as SummaryIcon } from './summary.svg';
+
+export { ReactComponent as GoBackIcon } from './go-back.svg';
