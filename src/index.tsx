5022d7b88c48b4c943bc8621f2a17a7cfa92b3b2
요술토끼 <curious.wzrabbit@gmail.com>
2024-08-19 21:25:26 +0900

    [CHORE] `axios`, `msw` 라이브러리를 설치하고 개발환경을 설정 (#22)
    * chore: `axios` 라이브러리를 `^1.7.4` 버전으로 설치
    
    Co-authored-by: baesee0806 <baesee@kakao.com>
    
    * chore: `msw` 라이브러리를 `^3.1.4` 버전으로 설치
    
    Co-authored-by: baesee0806 <baesee@kakao.com>
    
    - `Cannot read properties of undefined (reading 'url')` 에러가 msw의 최신 버전들에서 발생하고 있으며, Github Issue에서도 제대로 해결되지 않은 오류로 보입니다. 따라서 이 에러가 발생하기 전의 버전인 `^3.1.4` 버전으로 설치합니다.
    
    * chore: `msw`의 서비스 워커를 추가 및 작동하도록 개발환경 설정 진행, 테스트용 handler를 추가
    
    Co-authored-by: baesee0806 <baesee@kakao.com>
    
    ---------
    
    Co-authored-by: baesee0806 <baesee@kakao.com>

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,27 @@ import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
 
+const enableMocking = async () => {
+	if (process.env.NODE_ENV === 'production') {
+		return;
+	}
+
+	const { worker } = await import('./mocks/browser');
+
+	return worker.start();
+};
+
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
 
-root.render(
-	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<GlobalStyle />
-			<App />
-		</ThemeProvider>
-	</React.StrictMode>,
-);
+enableMocking().then(() => {
+	root.render(
+		<React.StrictMode>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<App />
+			</ThemeProvider>
+		</React.StrictMode>,
+	);
+});
