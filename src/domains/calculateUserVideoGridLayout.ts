3e368c5d6cf8204e88976ff353797b6c5dcb2946
WizardRabbit <curious.wzrabbit@gmail.com>
2024-08-25 00:30:35 +0900

    feat: 어떻게 유저 화면을 배치해야 하는지를 계산해주는 도메인 함수 구현

---
+++ src/domains/calculateUserVideoGridLayout.ts
@@ -0,0 +1,56 @@
+const MAX_COLUMN_COUNT = 1_000;
+const BASE_RATIO = 1_920 / 1_080;
+
+const calculateUserVideoGridLayout = (
+	videoGridWidth: number,
+	videoGridHeight: number,
+	videoGridLayoutGap: number,
+	participantsCount: number,
+) => {
+	let bestRatio = 0;
+	let layoutRowCount = 0;
+	let layoutColumnCount = 0;
+	let videoWidth = 0;
+	let videoHeight = 0;
+
+	for (let c = 1; c <= MAX_COLUMN_COUNT; c += 1) {
+		const r = Math.ceil(participantsCount / c);
+		const currentRatio = videoGridWidth / c / (videoGridHeight / r);
+
+		if (getRatioDifference(currentRatio) < getRatioDifference(bestRatio)) {
+			bestRatio = currentRatio;
+			layoutRowCount = r;
+			layoutColumnCount = c;
+			videoWidth = videoGridWidth / c;
+			videoHeight = videoGridHeight / r;
+		}
+	}
+
+	videoWidth = Math.min(videoWidth, videoHeight * BASE_RATIO);
+	videoHeight = Math.min(videoHeight, videoWidth / BASE_RATIO);
+
+	const rowVideoCounts = Array.from({ length: layoutRowCount }).map(
+		() => layoutColumnCount,
+	);
+	const emptySpacesCount =
+		(layoutRowCount * layoutColumnCount - participantsCount) %
+		layoutColumnCount;
+	const rowSize = rowVideoCounts.length;
+
+	for (let i = 0; i < emptySpacesCount; i += 1) {
+		rowVideoCounts[layoutRowCount - (i % layoutRowCount) - 1] -= 1;
+	}
+
+	return {
+		videoWidth,
+		videoHeight,
+		rowSize,
+		rowVideoCounts,
+	};
+};
+
+const getRatioDifference = (targetRatio: number) => {
+	return Math.max(targetRatio, BASE_RATIO) / Math.min(targetRatio, BASE_RATIO);
+};
+
+export default calculateUserVideoGridLayout;
