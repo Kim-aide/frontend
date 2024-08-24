6ed7d301003b73ec9e32ab71eb52db8a820abd9d
WizardRabbit <curious.wzrabbit@gmail.com>
2024-08-25 00:33:04 +0900

    feat: `<UserVideoGrid>` 컴포넌트를 구현

---
+++ src/components/UserVideoGrid/UserVideoGrid.styled.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	row-gap: 12px;
+	width: 100%;
+	height: 100%;
+`;
+
+const Row = styled.div`
+	display: flex;
+	justify-content: center;
+	column-gap: 12px;
+	width: 100%;
+	height: auto;
+`;
+
+export { Container, Row };
---
+++ src/components/UserVideoGrid/UserVideoGrid.tsx
@@ -0,0 +1,56 @@
+import { StreamManager } from 'openvidu-browser';
+import * as S from './UserVideoGrid.styled';
+import UserVideo from '../UserVideo/UserVideo';
+import useUserVideoGrid from '../../hooks/useUserVideoGrid';
+
+interface UserVideoInfo {
+	streamManager: StreamManager;
+	avatar: string;
+	nickname: string;
+}
+
+interface UserVideoGridProps {
+	userVideoInfos: UserVideoInfo[];
+}
+
+const UserVideoGrid = ({ userVideoInfos }: UserVideoGridProps) => {
+	const { videoWidth, videoHeight, rowVideoCounts, videoGridRef } =
+		useUserVideoGrid({
+			videoGridLayoutGap: 12,
+			participantsCount: userVideoInfos.length,
+		});
+	let renderingVideoIndex = 0;
+
+	console.log({ videoWidth, videoHeight, rowVideoCounts, videoGridRef });
+
+	return (
+		<S.Container ref={videoGridRef}>
+			{rowVideoCounts.map((rowVideoCount, rowIndex) => (
+				<S.Row key={rowIndex}>
+					{Array.from({ length: rowVideoCount }).map(() => {
+						if (!userVideoInfos[renderingVideoIndex]) {
+							return null;
+						}
+
+						const userVideoInfo = userVideoInfos[renderingVideoIndex];
+						const streamId =
+							userVideoInfos[renderingVideoIndex].streamManager.stream.streamId;
+
+						renderingVideoIndex += 1;
+
+						return (
+							<UserVideo
+								key={streamId}
+								width={`${videoWidth}px`}
+								height={`${videoHeight}px`}
+								{...userVideoInfo}
+							/>
+						);
+					})}
+				</S.Row>
+			))}
+		</S.Container>
+	);
+};
+
+export default UserVideoGrid;
