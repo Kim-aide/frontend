2e4ad90c552b65a42a68c106cecd76d873ace85a
WizardRabbit <curious.wzrabbit@gmail.com>
2024-09-06 23:56:02 +0900

    design: 모달의 UI 디자인을 시안대로 구현

--- src/components/ConfirmMeetingEndModal/ConfirmMeetingEndModal.styled.ts
+++ src/components/ConfirmMeetingEndModal/ConfirmMeetingEndModal.styled.ts
@@ -75,20 +75,112 @@ const Modal = styled.div`
 	position: relative;
 	display: flex;
 	flex-direction: column;
+	row-gap: 24px;
 	max-width: 100%;
 	max-height: 100%;
-	width: 400px;
-	height: 200px;
+	width: 448px;
+	border: 2px solid #282828;
+	border-radius: 8px;
 	box-shadow: 0 10px 10px ${({ theme }) => theme.colors.DARK_GRAY};
-	background-color: white;
+	background-color: #181818;
 
 	&.visible {
-		animation: ${zoomIn} 0.2s forwards;
+		animation: ${zoomIn} 0.15s forwards;
 	}
 
 	&.hidden {
-		animation: ${zoomOut} 0.2s forwards;
+		animation: ${zoomOut} 0.15s forwards;
 	}
 `;
 
-export { Container, Backdrop, Modal };
+const ModalHeader = styled.header`
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-end;
+	height: 54px;
+	padding: 0 24px;
+	border-radius: 6px 0;
+`;
+
+const Title = styled.span`
+	font-size: 28px;
+	color: white;
+	font-family: BMHanna11yrs;
+`;
+
+const CloseBtn = styled.button`
+	display: flex;
+	overflow: hidden;
+	width: 32px;
+	height: 32px;
+	color: #8d8d8d;
+	background: none;
+
+	& > svg {
+		width: 100%;
+		height: 100%;
+		transition: 0.15s;
+	}
+
+	&:hover > svg {
+		color: #bbbbbb;
+	}
+`;
+
+const ContentContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	row-gap: 8px;
+	padding: 0 24px;
+`;
+
+const Text = styled.p`
+	font-size: 16px;
+	color: white;
+`;
+
+const RoomNameText = styled.span`
+	color: #b7b7b7;
+	font-weight: 700;
+`;
+
+const ControlPanel = styled.div`
+	display: flex;
+	justify-content: flex-end;
+	column-gap: 8px;
+	height: 70px;
+	padding: 16px;
+	background-color: #1f1f1f;
+	border-radius: 0 0 6px 6px;
+`;
+
+const ControlPanelBtn = styled.button<{
+	$backgroundColor: string;
+	$hoverBackgroundColor: string;
+}>`
+	width: 88px;
+	height: 38px;
+	border-radius: 4px;
+	background-color: ${({ $backgroundColor }) => $backgroundColor};
+	color: white;
+	font-size: 15px;
+	transition: 0.15s;
+
+	&:hover {
+		background-color: ${({ $hoverBackgroundColor }) => $hoverBackgroundColor};
+	}
+`;
+
+export {
+	Container,
+	Backdrop,
+	Modal,
+	ModalHeader,
+	Title,
+	CloseBtn,
+	ContentContainer,
+	Text,
+	RoomNameText,
+	ControlPanel,
+	ControlPanelBtn,
+};
--- src/components/ConfirmMeetingEndModal/ConfirmMeetingEndModal.tsx
+++ src/components/ConfirmMeetingEndModal/ConfirmMeetingEndModal.tsx
@@ -1,6 +1,7 @@
 import * as S from './ConfirmMeetingEndModal.styled';
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
+import { CloseIcon } from '../../assets/svg';
 
 interface ConfirmMeetingEndModalProps {
 	roomName: string;
@@ -10,6 +11,7 @@ interface ConfirmMeetingEndModalProps {
 }
 
 const ConfirmMeetingEndModal = ({
+	roomName,
 	open,
 	onClose,
 	onConfirm,
@@ -45,8 +47,40 @@ const ConfirmMeetingEndModal = ({
 						className={isModalClosing ? 'hidden' : 'visible'}
 						onAnimationEnd={handleModalClosingAnimationEnd}
 					>
-						<button onClick={closeModal}>닫기</button>
-						<button onClick={confirmModal}>회의 종료</button>
+						<S.ModalHeader>
+							<S.Title>회의 종료 확인</S.Title>
+							<S.CloseBtn onClick={closeModal}>
+								<CloseIcon />
+							</S.CloseBtn>
+						</S.ModalHeader>
+						<S.ContentContainer>
+							<S.Text>
+								정말로 모든 참가자들을 내보내고,{' '}
+								<S.RoomNameText>{roomName}</S.RoomNameText>을(를) 종료할까요?
+							</S.Text>
+							<S.Text>
+								단순히 참가자님이 회의에서 나가시는 작업이 아니에요. 신중히
+								결정해주세요.
+							</S.Text>
+						</S.ContentContainer>
+						<S.ControlPanel>
+							<S.ControlPanelBtn
+								type="button"
+								$backgroundColor="#515151"
+								$hoverBackgroundColor="#656565"
+								onClick={closeModal}
+							>
+								취소
+							</S.ControlPanelBtn>
+							<S.ControlPanelBtn
+								type="button"
+								$backgroundColor="#ff4040"
+								$hoverBackgroundColor="#ff7c7c"
+								onClick={confirmModal}
+							>
+								회의 종료
+							</S.ControlPanelBtn>
+						</S.ControlPanel>
 					</S.Modal>
 				</S.Container>,
 				document.body,
