5a0b76a782eef21bde2f873cf42877394d06ca4b
WizardRabbit <curious.wzrabbit@gmail.com>
2024-09-12 22:40:39 +0900

    refactor: 모든 하위 컴포넌트들의 스토리북 상의 경로를 MeetingExitMenu로 설정

--- src/components/MeetingExitOptionsBubble/MeetingExitOptionsBubble.stories.tsx
+++ src/components/MeetingExitOptionsBubble/MeetingExitOptionsBubble.stories.tsx
@@ -5,7 +5,7 @@ import MeetingExitOptionsBubble from './MeetingExitOptionsBubble';
  * `<MeetingExitOptionsBubble>`은 사용자가 회의를 나갈 지, 모든 참가자에 대해 회의를 종료시킬 지를 선택할 수 있는 두 버튼이 담긴 컴포넌트입니다.
  */
 const meta: Meta<typeof MeetingExitOptionsBubble> = {
-	title: 'components/MeetingExitBtn/MeetingExitOptionsBubble',
+	title: 'components/MeetingExitMenu/MeetingExitOptionsBubble',
 	component: MeetingExitOptionsBubble,
 	argTypes: {
 		onMeetingExit: {
