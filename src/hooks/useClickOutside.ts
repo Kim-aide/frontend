a59e09557f826f2755a458f7100139071b8e084e
WizardRabbit <curious.wzrabbit@gmail.com>
2024-09-17 21:14:05 +0900

    refactor: 불필요한 console.log, 그리고 사용하지 않는 parameter를 JSDoc 주석에서 제거

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -10,7 +10,6 @@ interface UseClickOutsideParams {
  * @template T - 바깥쪽이 클릭되는 지를 감지할 때, 기준이 되는 요소의 타입입니다.
  *
  * @param {UseClickOutsideParams} params - 이 커스텀 훅이 받는 파라미터입니다.
- * @param {isActivated} - 이 커스텀 훅의 기능을 활성화시킬 지의 여부입니다. 요소가 비활성화되어 있는 등 // 여기부터
  * @param {() => void} params.onClickOutside - 바깥쪽이 클릭되는 것이 감지될 경우 실행시킬 콜백 함수입니다.
  */
 const useClickOutside = <T extends HTMLElement>({
@@ -19,8 +18,6 @@ const useClickOutside = <T extends HTMLElement>({
 	const clickOutsideRef = useRef<T>(null);
 
 	const executeCallbackIfClickOutside = (event: globalThis.MouseEvent) => {
-		console.log('execute!');
-
 		const clickOutsideElement = clickOutsideRef.current;
 
 		if (!clickOutsideElement || !(event.target instanceof Node)) {
@@ -28,7 +25,6 @@ const useClickOutside = <T extends HTMLElement>({
 		}
 
 		if (!clickOutsideElement.contains(event.target)) {
-			console.log('U clicked outside!');
 			onClickOutside();
 		}
 	};
