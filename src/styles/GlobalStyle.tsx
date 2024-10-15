9efa5cf1df6f427924d5d0e4c9b61c7f8df64199
WizardRabbit <curious.wzrabbit@gmail.com>
2024-10-15 21:33:01 +0900

    chore: `html`, `body`, `#root` 에 대한 크기 관련 글로벌 스타일을 작성

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -10,6 +10,17 @@ const baseStyle = css`
 		box-sizing: border-box;
 	}
 
+	html,
+	body {
+		width: 100%;
+		height: 100%;
+	}
+
+	#root {
+		width: 100%;
+		height: 100%;
+	}
+
 	button {
 		border: 0;
 
